b6d73622c402e4392b35b95f67b1a3c0d4c740dd
Jorge <jorge.naventa@ajegroup.com>
2025-10-28 17:05:10 -0500

    feat: add mixpanel to metrics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import mixpanel from "astrojs-mixpanel";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,9 @@ export default defineConfig({
   },
   output: "static",
   base: "web-toquea-campaign/",
+  integrations: [
+    mixpanel({
+      token: "daa7a6fb17dffa5f376bebb915c73c73",
+    }),
+  ],
 });
